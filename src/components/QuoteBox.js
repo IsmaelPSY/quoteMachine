6095885d38b032bf4a444b3b7a1624da57cf86c1
Deskrel <ismaelpsy.12@gmail.com>
2022-03-27 13:39:10 -0500

    Adding RandomQuotes Styles v0.2

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -2,28 +2,38 @@ import { useState } from 'react';
 import './QuoteBox.css';
 
 import Quotes from './quotes.json';
+import Styles from './styles.json'; 
 
 import QuoteText from './QuoteText';
 import QuoteAuthor from './QuoteAuthor';
 
 function QuoteBox() {
 
-  const randomInt = function() {
+  const randomQuote = function() {
     return Math.floor(Math.random() * (Quotes.quotes.length));
   } 
 
-  const [quote,setQuote] = useState(Quotes.quotes[randomInt()]);
-
-  function setStates(){
-    setQuote(Quotes.quotes[randomInt()]);
+  const randomStyle = function() {
+    return Math.floor(Math.random() * 5);
   }
 
+  const [quote,setQuote] = useState(Quotes.quotes[randomQuote()]);
+  const [style,setStyle] = useState(Styles.style[randomStyle()]);
+
+
+  function setStates(){
+    setQuote(Quotes.quotes[randomQuote()]);
+    setStyle(Styles.style[randomStyle()])
+  }
+  
     return (
-      <div className='container'>
-        <div className="quoteBox">
+      <div className={`container ${style.color1}`}>
+        <div className='quoteContainer'>
+        <div className={`quoteBox ${style.color2}`}>
           <QuoteText phrase = {quote.quote}/>
           <QuoteAuthor author = {quote.author}/>
           <button className='button' onClick={()=>setStates()}></button>
+        </div>
         </div>
       </div>
     );
